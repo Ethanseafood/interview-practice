93e70aa237263b1b540996879dc3a30d1a54a3ef
Ethan <ethangpe@gmail.com>
2022-08-11 15:19:55 +0800

    test project on github

--- my-project/js/main.js
+++ my-project/js/main.js
@@ -19,28 +19,3 @@
 // };
 
 // request.send();
-import fetch from 'node-fetch';
-async function getRepo() {
-  const clientIdAndSecret =
-    "Ethanseafood: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";
-  const base64 = Buffer.from(clientIdAndSecret).toString("base64");
-
-  const targetUrl = "https://api.github.com/users/Ethanseafood/repos"
-  //console.log(targetUrl);
-  const userDetails = await fetch(targetUrl, {
-    method: "GET",
-    headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-        "Accept-Language": "en_US",
-        Accept: "application/vnd.github.v3+json",
-        Authorization: `Basic ${base64}`,
-      },
-  });
-//   return userDetails.json();
-   return JSON.parse(userDetails);
-}
-
-(async () => {
-  const result = await getRepo();
-  console.log("users =", result.id);
-})();
